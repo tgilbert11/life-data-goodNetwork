f45ef1bbd5a39d6bc40e32b1bc8709ac325f3109
Taylor Gilbert <taylorg353@gmail.com>
2015-12-31 14:00:35 -0800

    Initial Commit

---
+++ life-data-goodNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  life-data-goodNetwork
+//
+//  Created by Taylor H. Gilbert on 12/31/15.
+//  Copyright © 2015 Taylor H. Gilbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
